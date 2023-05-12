6f89430df9eebff0b1715f57236ff5825a10adcf
Name <email@example.ru>
2023-05-12 18:52:55 +0300

    Add ApplicationDbContext from Context and add ConnectionStrings

---
+++ SocialNetwork.Data/Contecst/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using SocialNetwork.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Data.Contecst
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
