4f862be0af4909153dad530e6544ca542c4bbcf5
Name <email@example.ru>
2023-05-12 18:31:42 +0300

    Refactoring code

--- SocialNetwork.Contracts/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SocialNetwork.Contracts
-{
-    public class Class1
-    {
-    }
-}
--- SocialNetwork.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SocialNetwork.Data
-{
-    public class Class1
-    {
-    }
-}
