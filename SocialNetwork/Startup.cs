77c19c7853a5e7df6c9298f66474e24327716aa2
Name <email@example.ru>
2023-05-13 12:28:06 +0300

    Add main-page.css

--- SocialNetwork/Startup.cs
+++ SocialNetwork/Startup.cs
@@ -1,6 +1,7 @@
-using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
