00a0dd0f6fa3f0921966754d87751de4c912fcde
Name <email@example.ru>
2023-05-14 14:08:40 +0300

    Add RegisterController

---
+++ SocialNetwork/Controllers/RegisterController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Models;
+
+namespace SocialNetwork.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class RegisterController: Controller
+    {
+        [Route("Register")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return Vies("Home/Register");
+        }
+
+        [Route("RegisterPart2")]
+        [HttpGet]
+        public IActionResult RegisterPart2(RegisterViewModel model)
+        {
+            return View("RegisterPart2", model);
+        }
+    }
+}
